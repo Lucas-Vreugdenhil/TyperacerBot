c62e2e9c463b514b8b01d39e722f6d67170fefb0
Chappiemwoan <lucasvreugdenhil@gmail.com>
2022-02-10 14:19:14 +0100

    Update typeracerbot.py

--- typeracerbot.py
+++ typeracerbot.py
@@ -10,12 +10,12 @@ import pytesseract
 keyboard.wait('enter')
 
 #taking a screenshot of the text and assigning it to a variable
-screenshot = pyautogui.screenshot(region=(508, 605, 883, 200))
+screenshot = pyautogui.screenshot(region=(508, 505, 883, 200)) #coordinates may be different based on your screens resolution and your zoom.
 screenshot.save('image.png')
 image = Image.open('image.png')
 
 #reading the text of the screenshot
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' #this path may be different depending on your install location
 text = pytesseract.image_to_string(image)
 
 #cleaning up the text
